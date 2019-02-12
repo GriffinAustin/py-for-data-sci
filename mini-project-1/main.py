0e53faede079c522cd0513a52544bb8821523c37
GriffinAustin <gaustin25@gmail.com>
2019-02-12 11:43:28 -0700

    Update mini-project-1

--- mini-project-1/main.py
+++ mini-project-1/main.py
@@ -2,14 +2,28 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+# Question: Is there a correlation between country region and average hf score?
 def main():
     print('loading data...')
-    data = pd.read_csv('ks-projects-201801.csv') 
+    data = pd.read_csv('hfi_cc_2018.csv')
     print('data successfully loaded')
-    bar_graph = data.plot.bar(x='category', y='pledged', rot=0)
-    plt.show()
+    data = data.groupby('region')
+    final_data = list()
     
+    for name, group in data:
+        final_data.append((name, group['hf_score'].mean()))
+        
+    final_data.sort(key=lambda x:x[1])
+    x_data = list()
+    y_data = list()
+    [x_data.append(x[0]) for x in final_data]
+    [y_data.append(y[1]) for y in final_data]
+    
+    plt.bar(x_data, y_data)
+    plt.xlabel("Region")
+    plt.ylabel("Mean HF Score")
+    plt.xticks(rotation='vertical')
+    plt.show()
 
 if __name__ == '__main__':
     main()
