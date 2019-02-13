24c0fb99bea790ad4fb2642fbddd77b10f523a4f
GriffinAustin <gaustin25@gmail.com>
2019-02-13 10:57:02 -0700

    PEP8 conventions

--- mini-project-1/main.py
+++ mini-project-1/main.py
@@ -9,26 +9,26 @@ def main():
     print('data successfully loaded')
     data = data.groupby('region')
     final_data = list()
-    
+
     for name, group in data:
         final_data.append((name, group['hf_score'].mean()))
-        
-    final_data.sort(key=lambda x:x[1])
+
+    final_data.sort(key=lambda x: x[1])
     x_data = list()
     y_data = list()
     [x_data.append(x[0]) for x in final_data]
     [y_data.append(y[1]) for y in final_data]
-    
+
     plt.bar(x_data, y_data)
-    
+
     plt.title('Mean HF Score vs. Region')
-    plt.xlabel('Region')
+    plt.xlabel('Regiosn')
     plt.ylabel('Mean HF Score')
     plt.xticks(rotation='vertical')
     plt.ylim(0, 10)
-    
+
     plt.show()
-    
+
     # Saves graph
     #plt.savefig('meanhfscore_vs_region.png', bbox_inches='tight')
 
