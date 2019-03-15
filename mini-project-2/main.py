3d381e31c2472a01ce1c5951729521e09a2f6409
GriffinAustin <gaustin25@gmail.com>
2019-03-15 11:43:42 -0600

    Reinitialize mini project 2

--- mini-project-2/main.py
+++ mini-project-2/main.py
@@ -1,11 +1,23 @@
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import sklearn.model_selection
+
 
 def main():
-    data = pd.read_csv('mall_customers.csv')
-    df = pd.DataFrame([[data['Age'].tolist()], [data['Spending Score'].tolist()]]).transpose()
-    scatter = data.plot.scatter(x="Age", y="Spending Score")
+    data = pd.read_csv('iris.csv', names=['sepal length', 'sepal width',
+                                          'petal length', 'petal width',
+                                          'class'])
     
+    pd.plotting.scatter_matrix(data)
+    
+    arr = data.values
+    x = arr[: ,0:4]
+    y = arr[: ,4]
+    validation_size = 0.2
+    seed = 7
+    x_train, x_validation, y_train, y_validation = sklearn.model_selection.train_test_split(x, y, test_size=validation_size, random_state=seed)
+
+    # TODO: logistic regression, linear discriminant analysis, k-nearest neighbor
+    # classification and regression tree, Gaussian Naive Bayes, support vector machine
+
 if __name__ == '__main__':
     main()
