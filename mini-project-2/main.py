01386614f4029942ed75e10a5c66cc40cafa1a62
GriffinAustin <gaustin25@gmail.com>
2019-03-18 11:41:09 -0600

    Add algorithm evaluationa and comparison

--- mini-project-2/main.py
+++ mini-project-2/main.py
@@ -1,5 +1,46 @@
 import pandas as pd
-import sklearn.model_selection
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, KFold, cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+import random
+
+
+def algorithmize(data):
+    # Prepare training data
+    arr = data.values
+    x = arr[: ,0:4]
+    y = arr[: ,4]
+    validation_size = 0.2
+    seed = random.randint(1, 2**32-1)
+    x_train, x_validation, y_train, y_validation = train_test_split(
+            x, y, test_size=validation_size, random_state=seed)
+
+    # logistic regression, linear discriminant analysis, k-nearest neighbor
+    # classification and regression tree, Gaussian Naive Bayes, support 
+    # vector machine
+    models = [
+            ('LR', LogisticRegression(solver='liblinear', multi_class='ovr')),
+            ('LDA', LinearDiscriminantAnalysis()),
+            ('KNN', KNeighborsClassifier()),
+            ('CART', DecisionTreeClassifier()),
+            ('NB', GaussianNB()),
+            ('SVM', SVC(gamma='auto'))
+            ]
+    results = []
+    names= []
+    # Evaluate using each algorithm
+    for name, model in models:
+        kfold = KFold(n_splits=10, random_state=seed)
+        cv_results = cross_val_score(model, x_train, y_train, cv=kfold, scoring='accuracy')
+        results.append(cv_results)
+        names.append(name)
+        
+    return (results, names)
 
 
 def main():
@@ -9,15 +50,17 @@ def main():
     
     pd.plotting.scatter_matrix(data)
     
-    arr = data.values
-    x = arr[: ,0:4]
-    y = arr[: ,4]
-    validation_size = 0.2
-    seed = 7
-    x_train, x_validation, y_train, y_validation = sklearn.model_selection.train_test_split(x, y, test_size=validation_size, random_state=seed)
-
-    # TODO: logistic regression, linear discriminant analysis, k-nearest neighbor
-    # classification and regression tree, Gaussian Naive Bayes, support vector machine
+    evaluation = algorithmize(data)
+        
+    # Compare algorithms
+    fig = plt.figure()
+    fig.suptitle('Algorithm Comparison')
+    ax = fig.add_subplot(111)
+    plt.boxplot(evaluation[0])
+    ax.set_xticklabels(evaluation[1])
+    plt.show()
+    
+    # TODO: Make predictions
 
 if __name__ == '__main__':
     main()
